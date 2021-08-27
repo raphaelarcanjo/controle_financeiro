ec542d458fea37018c3173f0edd391ec8f207932
Raphael Oliveira Cunha <raphael_arcanjo@live.com>
2021-08-27 17:53:52 -0300

    conteúdo das views

--- controle/urls.py
+++ controle/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),
+    path('', views.home, name='home'),
+    path('entrada', views.entrada, name='entrada'),
+    path('saida', views.saida, name='saida'),
+    path('contato', views.contato, name='contato'),
 ]
--- controle/views.py
+++ controle/views.py
@@ -1,6 +1,28 @@
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-def Home(request):
-    data = {}
+def home(request):
+    data = {
+        'entrada': 0,
+        'saida': 0,
+        'caixa': 0
+    }
     return render(request, 'home.html', data)
+
+
+def entrada(request):
+    data = {
+        'entrada': 0
+    }
+    return render(request, 'entrada.html', data)
+
+
+def saida(request):
+    data = {
+        'saida': 0
+    }
+    return render(request, 'saida.html', data)
+
+
+def contato(request):
+    return render(request, 'contato.html')
