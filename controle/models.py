74568bfadc7270809e70aca8c816689d8d4ca27e
Raphael Oliveira Cunha <raphael_arcanjo@live.com>
2021-08-31 14:05:26 -0300

    Registro de entrada e saída

--- controle/models.py
+++ controle/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime, date
 
-# Create your models here.
+
+class Entrada(models.Model):
+    valor = models.DecimalField(decimal_places=2, max_digits=8)
+    data = models.DateField(null=True, default=datetime.now().date())
+    fonte = models.CharField(max_length=80)
+    extra = models.BooleanField(null=True, default=False)
+
+    def __str__(self):
+        return self.valor
+
+
+class Saida(models.Model):
+    valor = models.DecimalField(decimal_places=2, max_digits=8)
+    data = models.DateField(null=True, default=datetime.now().date())
+    fonte = models.CharField(max_length=80)
+    superfluo = models.BooleanField(null=True, default=False)
+
+    def __str__(self):
+        return self.valor
--- controle/views.py
+++ controle/views.py
@@ -1,31 +1,64 @@
 from django.http.response import HttpResponse, JsonResponse
+from django.db.models import Sum
 from django.shortcuts import render
+from . import models
 
 def home(request):
+    entrada = models.Entrada.objects.all().aggregate(Sum('valor'))['valor__sum']
+    entrada = 0 if entrada is None else entrada
+    saida = models.Saida.objects.all().aggregate(Sum('valor'))['valor__sum']
+    saida = 0 if saida is None else saida
+    total = entrada - saida
     data = {
-        'entrada': 0,
-        'saida': 0,
-        'caixa': 0
+        'entrada': entrada,
+        'saida': saida,
+        'total': total
     }
     return render(request, 'home.html', data)
 
 
 def entrada(request):
+    form_entrada = {}
     if request.method == 'POST':
-        pass
-    entrada = request.POST
+        entrada = models.Entrada()
+        entrada.valor = request.POST.get('valor')
+        entrada.data = request.POST.get('data')
+        entrada.fonte = request.POST.get('fonte')
+        entrada.extra = True if request.POST.get('extra') else False
+        try:
+            entrada.save()
+        except Exception as e:
+            print(e)
+            form_entrada = request.POST
+    entradas = models.Entrada.objects.all()
+    total = models.Entrada.objects.all().aggregate(Sum('valor'))['valor__sum']
     data = {
-        'entrada': entrada
+        'form_entrada': form_entrada,
+        'entradas': entradas,
+        'total': 0 if total is None else total
     }
     return render(request, 'entrada.html', data)
 
 
 def saida(request):
+    form_saida = {}
     if request.method == 'POST':
-        pass
-    saida = request.POST
+        saida = models.Saida()
+        saida.valor = request.POST.get('valor')
+        saida.data = request.POST.get('data')
+        saida.fonte = request.POST.get('fonte')
+        saida.extra = True if request.POST.get('extra') else False
+        try:
+            saida.save()
+        except Exception as e:
+            print(e)
+            form_saida = request.POST
+    saidas = models.Saida.objects.all()
+    total = models.Saida.objects.all().aggregate(Sum('valor'))['valor__sum']
     data = {
-        'saida': saida
+        'form_saida': form_saida,
+        'saidas': saidas,
+        'total': 0 if total is None else total
     }
     return render(request, 'saida.html', data)
 
